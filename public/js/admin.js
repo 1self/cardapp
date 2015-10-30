b1dd63399ae3190c51e732210b767427f3e3122a
Martin <martinstrotton@gmail.com>
2015-10-30 12:01:48 +0000

    fix for admin page to work with cardsgenerating and datasyncing card types

--- public/js/admin.js
+++ public/js/admin.js
@@ -31,7 +31,6 @@ function showCards($cardsContent) {
 }
 
 function buildCardDetail(cardsArray, $cardsContent) {
-    var lastDate = '';
     var $activeDateSection;
     var cardsPerDate;
     var datesCount = 0;
@@ -46,13 +45,17 @@ function buildCardDetail(cardsArray, $cardsContent) {
             cardsPerDate = 0;
             $activeDateSection = addDateSection(cardData, $cardsContent);
         } else {
+            if ($activeDateSection === undefined) {
+                cardsPerDate = 0;
+                datesCount++;
+            }
             cardsPerDate++;
-            addCardDetail(cardData, $activeDateSection);
+            $activeDateSection = addCardDetail(cardData, $cardsContent, $activeDateSection);
         }
     }
 
     if ($activeDateSection !== undefined) {
-        $activeDateSection.find('.card-count').text('Cards: ' + cardsPerDate);
+        $activeDateSection.find('.card-qty').text('Cards: ' + cardsPerDate);
     }
 
     $('.total-days').text(datesCount + ' days');
@@ -67,19 +70,32 @@ function addDateSection(cardData, $cardsContent) {
     return $dateSection;
 }
 
-function addCardDetail(cardData, $dateSection) {
+function addCardDetail(cardData, $cardsContent, $dateSection) {
     var $cardRow = $('.card-row.template').clone();
     var $separator = $('.card-separator.template').clone();
     $cardRow.removeClass('template');
     $separator.removeClass('template');
-    $cardRow.find('.service').addClass(getDataSource(cardData));
 
-    createCardText(cardData);
+    if ($dateSection === undefined) {
+        $dateSection = addDateSection(cardData, $cardsContent);
+    }
 
-    $cardRow.find('.card-text').html(cardData.cardText.description);
-    $cardRow.find('.card-comparitor').html(cardData.cardText.comparitor);
+    if (cardData.type === "datasyncing" || cardData.type === "cardsgenerating") {
+        setDataSource(cardData);
+        $cardRow.find('.service').addClass(cardData.identifier);
+        $cardRow.find('.card-text').html(cardData.type);
+    } else {
+        $cardRow.find('.service').addClass(getDataSource(cardData));
+
+        createCardText(cardData);
+
+        $cardRow.find('.card-text').html(cardData.cardText.description);
+        $cardRow.find('.card-comparitor').html(cardData.cardText.comparitor);
+    }
 
     $cardsDiv = $dateSection.find('.cards-for-date');
     $cardsDiv.append($cardRow);
     $cardsDiv.append($separator);
+
+    return $dateSection;
 }
