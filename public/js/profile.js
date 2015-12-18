82d024287b72ac29c341be37ca52ca37ffd13ce5
Ed Sykes <ed.sykes@gmail.com>
2015-12-18 13:49:10 +0000

    added analytics for the rest of the pages and added an event for when the user clicks logout

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
 function executeOnLoadTasks() { 
 
     $('.logout-button').click(function() {
+    	var ev = {
+    		eventCategory: 'profile',
+    		eventAction: 'logout',
+    		eventLabel: ''
+    	};
+
+    	analytics.send('event', ev);
         window.location.href = '/logout';
         return false;
     });
